5831ed725b71312e90d20f68c824eb28786791a3
hainv <hai213k57@gmail.com>
2019-06-14 11:35:06 +0700

    Create code base html
    Signed-off-by: hainv <hai213k57@gmail.com>

--- chat-io/server/src/routes/root.js
+++ chat-io/server/src/routes/root.js
@@ -2,10 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 // root
-router.get('/', function (req, res) {
-    res.json({
-        msg: 'Welcome to service chatio.'
-    });
+// router.get('/', function (req, res) {
+//     res.json({
+//         msg: 'Welcome to service chatio.'
+//     });
+// });
+
+router.get('/', function(req, res, next) {
+	res.render('login.html');
+});
+
+router.get('/roomsList', function(req, res, next) {
+	res.render('rooms.html');
+});
+
+router.get('/chatroom', function(req, res, next) {
+	res.render('chatroom.html');
 });
 
 require('./user')(router);
--- chat-io/server/src/server.js
+++ chat-io/server/src/server.js
@@ -1,11 +1,22 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
+const path = require('path');
 
 const Config = require('./config/config');
 
 const port = Config.PORT_SEVER;
 const app = express();
+
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+
+// Middleware to catch 404 errors
+// app.use(function (req, res, next) {
+//     res.status(404).sendFile(process.cwd() + '/views/404.html');
+// });
 
 // create socket io
 const { createSocketIO } = require('./socket.io/socket-io');
@@ -15,6 +26,7 @@ app.use(cors(), bodyParser.json());
 
 // set static folder
 app.use('/chatio/files', express.static('src/public'));
+app.use('/chatio', express.static('src/public'));
 
 // import router
 const rootRouter = require('./routes/root');
