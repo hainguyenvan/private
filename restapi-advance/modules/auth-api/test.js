d3cfc17972078043d081f01eb224f86c451b3d28
hainv <hai213k57@gmail.com>
2019-07-12 09:02:16 +0700

    Add modules auth api in rest api advance
    Signed-off-by: hainv <hai213k57@gmail.com>

--- restapi-advance/modules/auth-api/test.js
+++ restapi-advance/modules/auth-api/test.js
@@ -1,18 +1,23 @@
-const { generateKey, encryptBcrypt, enscriptCrypto, descriptCrypto, isValidateSecret } = require('./index');
+const { generateKeyRSA, encryptBcrypt, enscriptCrypto, descriptCrypto, isValidateSecret } = require('./index');
 
-// generateKey();
+// generateKeyRSA();
 
-let enscript = enscriptCrypto('hihi');
+let data = {
+    id: 1,
+    timestamp: 1562839200
+};
+data.secret = encryptBcrypt(JSON.stringify(data));
+
+let enscript = enscriptCrypto(JSON.stringify(data));
 let descript = descriptCrypto(enscript);
-let enscriptBcryptData = encryptBcrypt('hihi');
 
 console.log('enscript:', enscript);
 console.log('descript: ', descript);
 
-isValidateSecret('hihi', enscriptBcryptData)
-    .then(status => {
-        console.log('status validate secret: ', status);
-    })
-    .catch(err => {
-        console.log(err);
-    })
+// isValidateSecret('hihi', enscriptBcryptData)
+//     .then(status => {
+//         console.log('status validate secret: ', status);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
--- restapi-advance/src/middlewares/auth-api.js
+++ restapi-advance/src/middlewares/auth-api.js
@@ -1,26 +1,4 @@
-const crypto = require("crypto");
-const bcrypt = require('bcrypt');
-
-const Config = require('../config/config');
-const RSAKey = require('../config/rsa-key');
-
-const encryptString = function(toEncrypt) {
-    let buffer = Buffer.from(toEncrypt);
-    let encrypted = crypto.publicEncrypt(RSAKey.PUBLIC_KEY, buffer);
-    return encrypted.toString("base64");
-};
-
-const encryptBcrypt = function(data) {
-    let hash = bcrypt.hashSync(data, Config.SALT_ROUNDS);
-    return hash;
-}
-
-const decryptString = function(toDecrypt) {
-    let buffer = Buffer.from(toDecrypt, "base64");
-    let decrypted = crypto.privateDecrypt(RSAKey.PRIAVTE_KEY, buffer);
-    return decrypted.toString("utf8");
-};
-
+const { descriptCrypto, isValidateSecret } = require('../../modules/auth-api/index');
 
 exports.authAPI = async function(req, res, next) {
     // let apiKey = req.headers['api-key'];
@@ -32,7 +10,7 @@ exports.authAPI = async function(req, res, next) {
     //     });
     //     return;
     // }
-    // let decryptData = decryptString(apiKey);
+    // let decryptData = descriptCrypto(apiKey);
     // try {
     //     decryptData = JSON.parse(decryptData);
     //     let timestamp = decryptData.timestamp;
@@ -40,7 +18,17 @@ exports.authAPI = async function(req, res, next) {
     //     payload.timestamp = timestamp;
 
     //     let payloadPlanText = JSON.stringify(payload);
-    //     let match = await bcrypt.compare(payloadPlanText, secret);
+
+    //     // check payload hash with bcrypt
+    //     // let match = await bcrypt.compare(payloadPlanText, secret);
+    //     let match = await isValidateSecret(payloadPlanText, secret)
+    //         .then(status => {
+    //             return status;
+    //         })
+    //         .catch(err => {
+    //             console.log(err);
+    //             return false;
+    //         })
 
     //     if (!match) {
     //         res.send({
