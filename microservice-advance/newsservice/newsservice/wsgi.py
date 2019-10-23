a017194bf7638d911fb4413de6607fd37a47ce97
hainv <hai213k57@gmail.com>
2019-10-23 22:49:39 +0700

    Create examples news service
    Signed-off-by: hainv <hai213k57@gmail.com>

--- microservice-advance/newsservice/newsservice/wsgi.py
+++ microservice-advance/newsservice/newsservice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for newsservce project.
+WSGI config for newsservice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsservce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsservice.settings')
 
 application = get_wsgi_application()
